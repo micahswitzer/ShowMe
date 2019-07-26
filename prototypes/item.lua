663b8271fa9db96336c48413bfdeba667ce69f31
Micah Switzer <micah.switzer.dev@outlook.com>
2019-07-25 21:17:01 -0500

    Small cleanups

--- prototypes/item.lua
+++ prototypes/item.lua
@@ -2,7 +2,7 @@ local showmeTool = table.deepcopy(data.raw["copy-paste-tool"]["copy-paste-tool"]
 
 showmeTool.name = "showme-tool"
 showmeTool.type = "selection-tool"
-showmeTool.selection_mode = {"any-tile"}
+showmeTool.selection_mode = {"nothing"}
 showmeTool.alt_selection_mode = {"nothing"}
 showmeTool.selection_cursor_box_type = "entity"
 showmeTool.show_in_library = true
