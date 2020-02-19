d47964e05edc6021ed93bc9a2bf04584829a15cd
Micah Switzer <micah.switzer.dev@outlook.com>
2020-02-19 17:15:47 -0500

    Updated to Factorio 0.18

---
+++ migrations/ensure-globals.lua
@@ -0,0 +1,12 @@
+if global.boxes == nil then
+    global.boxes = {}
+end
+if global.hover_targets == nil then
+    global.hover_targets = {}
+end
+if global.to_display == nil then
+    global.to_display = {}
+end
+if global.cursor == nil then
+    global.cursor = {}
+end
